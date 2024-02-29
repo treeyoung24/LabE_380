f9e4c6180ba882ea3d05d14ab01952edf7d60ea2
Aidan <aidanhuang10@gmail.com>
2024-02-29 15:10:57 -0700

    Finished ToDoList

--- ToDoList.java
+++ ToDoList.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 public class ToDoList implements IToDoList {
     //Data Members
@@ -11,27 +10,38 @@ public class ToDoList implements IToDoList {
     }
 
     public void undo() {
-        
+        pushStack();
+        //Gets the most recent edit on the taskList and sets the list equal to
+        //The most recent edit done to the list of tasks
+        this.taskList = history.get(0); 
     }
 
     public void addTask(Task task) {
+        pushStack();
         this.taskList.add(task);
     }
 
     public void completeTask(String id) {
+        pushStack();
         this.taskList.get(Integer.parseInt(id)).setIsCompleted(true);
     }
 
     public void deleteTask(String id) {
+        pushStack();
         this.taskList.remove(Integer.parseInt(id));
     }
 
     public void editTask(String id, String title, Boolean isCompleted) {
+        pushStack();
         this.taskList.get(Integer.parseInt(id)).setTitle(title);
         this.taskList.get(Integer.parseInt(id)).setIsCompleted(isCompleted);
     }
 
     public List<Task> listTasks() {
         return this.taskList;
+    }
+
+    public void pushStack() {
+        history.push(this.taskList);
     }
 }
