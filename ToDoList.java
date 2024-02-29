a4420a677f13b4d42ee328a25139e2224698c8ca
Aidan <aidanhuang10@gmail.com>
2024-02-29 15:00:01 -0700

    Finished add, complete and delete

--- ToDoList.java
+++ ToDoList.java
@@ -5,24 +5,30 @@ public class ToDoList implements IToDoList {
     private List<Task> taskList;
     private Stack<List<Task>> history = new Stack<List<Task>>();
 
-    public void undo() {
+    public ToDoList() {
+        this.taskList = null;
+        this.history = null;
+    }
 
+    public void undo() {
+        
     }
 
     public void addTask(Task task) {
-        this.taskList.
+        this.taskList.add(task);
     }
 
     public void completeTask(String id) {
-        
+        this.taskList.get(Integer.parseInt(id)).setIsCompleted(true);
     }
 
     public void deleteTask(String id) {
-        
+        this.taskList.remove(Integer.parseInt(id));
     }
 
     public void editTask(String id, String title, Boolean isCompleted) {
-        
+        this.taskList.get(Integer.parseInt(id)).setTitle(title);
+        this.taskList.get(Integer.parseInt(id)).setIsCompleted(isCompleted);
     }
 
     public List<Task> listTasks() {
